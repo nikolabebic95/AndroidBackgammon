ff7e128b62113534e7460b6e008e862cf8e22dca
Nikola Bebic <bn140314d@student.etf.bg.ac.rs>
2018-02-16 18:46:00 +0100

    Basic start activity

--- Backgammon/app/src/main/java/rs/ac/bg/etf/pmu/bn140314d/backgammon/gui/SettingsActivity.java
+++ Backgammon/app/src/main/java/rs/ac/bg/etf/pmu/bn140314d/backgammon/gui/SettingsActivity.java
@@ -1,27 +1,25 @@
 package rs.ac.bg.etf.pmu.bn140314d.backgammon.gui;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import rs.ac.bg.etf.pmu.bn140314d.backgammon.R;
-import rs.ac.bg.etf.pmu.bn140314d.backgammon.persistence.Persistence;
 
-public class StartPageActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 100;
-
     private final Handler mHideHandler = new Handler();
     private View mContentView;
 
     // Delayed removal of status and navigation bar
+
     // Note that some of these constants are new as of API 16 (Jelly Bean)
     // and API 19 (KitKat). It is safe to use them, as they are inlined
     // at compile-time and do nothing on earlier devices.
@@ -33,38 +31,11 @@ public class StartPageActivity extends AppCompatActivity {
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
-    public void onStartGameClicked(View view) {
-        Persistence.clearCurrentGame();
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-    }
-
-    public void onContinueClicked(View view) {
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-    }
-
-    public void onSettingsClicked(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
-    public void onHallOfFameClicked(View view) {
-        Intent intent = new Intent(this, HallOfFameActivity.class);
-        startActivity(intent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_page);
+        setContentView(R.layout.activity_game);
         mContentView = findViewById(R.id.fullscreen_content);
-
-        View continueButton = findViewById(R.id.continue_button);
-
-        if (!Persistence.checkIfStartedGameExists()) {
-            continueButton.setEnabled(false);
-        }
     }
 
     @Override
---
+++ Backgammon/app/src/main/java/rs/ac/bg/etf/pmu/bn140314d/backgammon/persistence/Persistence.java
@@ -0,0 +1,24 @@
+package rs.ac.bg.etf.pmu.bn140314d.backgammon.persistence;
+
+/**
+ * Utility class used for accessing persistent data
+ */
+public final class Persistence {
+    /**
+     * Prevent instantiating of the utility class
+     */
+    private Persistence() {}
+
+    /**
+     * Checks if there is already a started game
+     * @return Whether a started game exists
+     */
+    public static boolean checkIfStartedGameExists() {
+        // TODO: Implementation
+        return false;
+    }
+
+    public static void clearCurrentGame() {
+        // TODO: Implementation
+    }
+}
